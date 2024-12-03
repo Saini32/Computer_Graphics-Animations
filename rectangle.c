d0afefe9c3a150d4d12cfc68621e647e3cf6a53f
Sneha Mourya <157492674+Saini32@users.noreply.github.com>
2024-12-03 17:51:57 +0530

    03/12/24

---
+++ rectangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#include<graphics.h>
+void main()
+{
+int gd=DETECT,gm;
+clrscr();
+initgraph(&gd,&gm,"C:\\turboc3\\bgi");
+rectangle(150,120,420,280);
+getch();
+closegraph();
+}

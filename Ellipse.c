43503ed2cee29ce3fca38848818784a752722522
Sneha Mourya <157492674+Saini32@users.noreply.github.com>
2024-12-03 18:40:06 +0530

    03/12/24

---
+++ Ellipse.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#include<graphics.h>
+void main()
+{
+int gd=DETECT,gm;
+clrscr();
+initgraph(&gd,&gm,"C:\\turboc3\\bgi");
+ellipse(250,200,0,360,100,50);
+getch();
+closegraph();
+}
